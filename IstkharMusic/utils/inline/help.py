936f943eea25cde0e9726292b06182163d9cfc30
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-04 19:34:11 +0530

    Update help.py

--- IstkharMusic/utils/inline/help.py
+++ IstkharMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from IstkharMusic import app
+from MahiMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
