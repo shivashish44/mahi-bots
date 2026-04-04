6af4e759c3407c003e050cfcd8b8c58c41cdb8e6
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-04 20:08:30 +0530

    Update shuffle.py

--- IstkharMusic/plugins/admins/shuffle.py
+++ IstkharMusic/plugins/admins/shuffle.py
@@ -2,10 +2,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from IstkharMusic import app
-from IstkharMusic.misc import db
-from IstkharMusic.utils.decorators import AdminRightsCheck
-from IstkharMusic.utils.inline import close_markup
+from MahiMusic import app
+from MahiMusic.misc import db
+from MahiMusic.utils.decorators import AdminRightsCheck
+from MahiMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
