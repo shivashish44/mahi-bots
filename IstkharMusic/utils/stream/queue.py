1c00813ca59ab2c0275713cff64c6f7e05156814
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-04 19:29:38 +0530

    Update import paths for MahiMusic module

--- IstkharMusic/utils/stream/queue.py
+++ IstkharMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from IstkharMusic.misc import db
-from IstkharMusic.utils.formatters import check_duration, seconds_to_min
+from MahiMusic.misc import db
+from MahiMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
