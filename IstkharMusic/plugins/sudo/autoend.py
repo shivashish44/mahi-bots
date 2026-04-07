6e0d32e6827e7430ee36f299f514222da8f41ea3
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-08 02:00:37 +0530

    BETA BOTS

--- IstkharMusic/plugins/sudo/autoend.py
+++ IstkharMusic/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from IstkharMusic import app
-from IstkharMusic.misc import SUDOERS
-from IstkharMusic.utils.database import autoend_off, autoend_on
+from MahiMusic import app
+from MahiMusic.misc import SUDOERS
+from MahiMusic.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
