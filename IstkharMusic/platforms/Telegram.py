31d80fb3a01f208bc0c24a0e73dc1a54f13134a8
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-08 00:51:17 +0530

    BETA BOTS

--- IstkharMusic/platforms/Telegram.py
+++ IstkharMusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from IstkharMusic import app
-from IstkharMusic.utils.formatters import (
+from MahiMusic import app
+from MahiMusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
